d33773c4dfd82caecd4fb32475be2adcaa1fede1
rmsrahat <rmsrahat2002@gmail.com>
2026-05-13 21:28:46 +0600

    Updated db.php with PDO connection

--- config/db.php
+++ config/db.php
@@ -1,17 +1,14 @@
 <?php
 
-    $host       = "127.0.0.1";
-    $dbuser     = "root";
-    $dbpassword = "";
-    $dbname     = "book_store";
+$host       = "127.0.0.1";
+$dbuser     = "root";
+$dbpassword = "";
+$dbname     = "book_store";
 
-    function getConnection(){
-        global $host, $dbuser, $dbpassword, $dbname;
-        $con = mysqli_connect($host, $dbuser, $dbpassword, $dbname);
-        if(!$con){
-            die("Connection failed: " . mysqli_connect_error());
-        }
-        return $con;
-    }
-
-?>
+function getConnection() {
+    global $host, $dbuser, $dbpassword, $dbname;
+    $dsn = "mysql:host=$host;dbname=$dbname";
+    $pdo = new PDO($dsn, $dbuser, $dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $pdo;
+}
