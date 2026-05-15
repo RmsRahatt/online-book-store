75b8a5bd7172f5ae03c61bea83bee31a9e1e8d2e
rmsrahat <rmsrahat2002@gmail.com>
2026-05-15 21:13:57 +0600

    Add Navbar.php File

--- view/admin/navbar.php
+++ view/admin/navbar.php
@@ -3,9 +3,10 @@
     <div class="navbar-links">
         <a href="dashboard.php">Dashboard</a>
         <a href="book_list.php">Books</a>
+        <a href="category_list.php">Categories</a>
         <a href="customer_list.php">Customers</a>
         <a href="user_list.php">All Users</a>
-        <a href="purchase_history.php">Purchase History</a>
+        <a href="purchase_history.php">Orders</a>
         <a href="../../controller/logout.php">Logout</a>
-</div>
+    </div>
 </nav>
